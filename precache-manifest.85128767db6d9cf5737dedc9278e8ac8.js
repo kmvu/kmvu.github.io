7428b86f452431c578a8282c5d49a87f8bdb3fea
Khang Vu <kmvu91@gmail.com>
2020-05-27 16:37:37 +0800

    Updates

---
+++ precache-manifest.85128767db6d9cf5737dedc9278e8ac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30bf8261a7d63d529d9e5f35a329d43b",
+    "url": "/react-native/index.html"
+  },
+  {
+    "revision": "d94c0226a05bbd8af874",
+    "url": "/react-native/static/css/main.b044d04a.chunk.css"
+  },
+  {
+    "revision": "5b1361c9f9c34e154077",
+    "url": "/react-native/static/js/2.286c2ded.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-native/static/js/2.286c2ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94c0226a05bbd8af874",
+    "url": "/react-native/static/js/main.9167ef4b.chunk.js"
+  },
+  {
+    "revision": "141992efdda776516459",
+    "url": "/react-native/static/js/runtime-main.80abc795.js"
+  }
+]);
